c798043ffdb2ac61aeae29a91218c19817595cb9
togana <o.togana@gmail.com>
2020-01-06 20:25:41 +0900

    add echo

--- src/echo/main.go
+++ src/echo/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+var omitNewline = flag.Bool("n", false, "Do not print the trailing newline character.")
+
+const (
+	Space   = " "
+	Newline = "\n"
+)
 
 func main() {
-	fmt.Printf("Hello World echo!\n")
+	flag.Parse()
+
+	var s string = ""
+
+	for i := 0; i < flag.NArg(); i++ {
+		if i > 0 {
+			s += Space
+		}
+		s += flag.Arg(i)
+	}
+
+	if !*omitNewline {
+		s += Newline
+	}
+	fmt.Print(s)
 }
